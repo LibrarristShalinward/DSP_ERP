eb7f160f3fafab9e547c95b5389a12d9870c8ddb
LibrarristShalinward <jbh23@mails.tsinghua.edu.cn>
2025-05-31 16:21:43 +0800

    feat(recipe): 添加计算生产力和需求产量的函数

--- dsp/recipe.py
+++ dsp/recipe.py
@@ -113,6 +113,43 @@ class Recipe:
         return not self.all_objs_satisfies(
             lambda i: not cri(i)
         )
+    
+    # region 计算函数
+    def result2prod(self, 
+                result: Item, scale: float
+            ) -> float: 
+        """根据产物产量计算配方所需生产力
+
+        Args:
+            result (Item): 目标产物
+            scale (float): 目标产量(单位/min)
+
+        Returns:
+            float: 生产力(单位为一台基础设施的生产力)
+        """
+        if result in self.results.keys(): 
+            rs = self.results[result]
+            if result in self.items.keys(): 
+                rs -= self.items[result]
+        else: 
+            assert False, f"【{result}】不是合成路线的产物"
+        return scale * self.time / rs / 3600.
+    
+    def prod2require(self, 
+                raw: Item, scale: float
+            ) -> float: 
+        """根据生产力计算配方中原料的需求产量
+
+        Args:
+            raw (Item): 目标原料
+            scale (float): 生产力(单位为一台基础设施的生产力)
+
+        Returns:
+            float: 原料需求产量(单位/min)
+        """
+        return scale * 3600. * self.items[raw] / self.time
+    
+    # endregion
 
 dsp_recipes = {
     rc.id: rc for rc in sorted(
