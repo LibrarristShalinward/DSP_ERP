7c88f10303718266521b889e3c444d021027620b
LibrarristShalinward <jbh23@mails.tsinghua.edu.cn>
2025-05-31 20:46:21 +0800

    feat(frontend): 添加物品按钮组件

---
+++ frontend/components/item.py
@@ -0,0 +1,22 @@
+from dsp import *
+import dearpygui.dearpygui as dpg
+from .icon import dsp_icons
+
+
+class DPGItemButton: 
+    def __init__(self, 
+                item: Item, 
+                pos: tuple[float, float], 
+                size: float, 
+                rounding: float = .1, 
+                parent: int | str = 0
+            ) -> None:
+        self.icon = dsp_icons[item]
+        dp = .5 - rounding
+        with dpg.draw_node(label = "icon_node", parent = parent) as self.tag: 
+            self.button = dpg.draw_rectangle((-.5, -.5), (.5, .5), fill = self.icon.color, rounding = rounding * size)
+            self.texture = dpg.draw_image(self.icon.tag,  (-dp, -dp), (dp, dp))
+        dpg.apply_transform(
+            self.tag, 
+            dpg.create_translation_matrix(pos) * dpg.create_scale_matrix((size, size))
+        )
